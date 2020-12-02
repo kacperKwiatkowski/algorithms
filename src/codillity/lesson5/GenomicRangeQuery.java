612eac00ee61a37d3f93f47e175c4633826c65ab
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-12-02 13:33:41 +0100

    Lesson 5 / GenomicRangeQuery done, but not efficient.

---
+++ src/codillity/lesson5/GenomicRangeQuery.java
@@ -0,0 +1,49 @@
+package codillity.lesson5;
+
+import utils.Printer;
+
+public class GenomicRangeQuery {
+    public static void main(String[] args){
+
+        //A = 1
+        //C = 2
+        //G = 3
+        //T = 4
+
+        //S is the DNA sequence
+        //K is arr index
+        //K is < than 0 and M
+        //M is arr length
+        //P[K] and Q[K] are indexes of S
+        //P[K] and Q[K] are range in S
+        //impact factor is the lowest nucleotide
+
+        Printer.printArray(
+                solution("CAGCCTA", new int[] {2, 5, 0},   new int[] {4, 5, 6}));
+        Printer.printArray(
+                solution("CAGCCTA", new int[] {6, 1, 0},   new int[] {6, 2, 4}));
+        Printer.printArray(
+                solution("CAGCCTA", new int[] {2, 4, 3},   new int[] {3, 5, 3}));
+    }
+
+    public static int [] solution(String S, int[] P, int[] Q){
+
+        //The returned result will have length of any given sequence
+        int [] result = new int [P.length];
+
+        for(int index = 0; index < result.length; index++){
+
+            if(S.substring(P[index], Q[index] + 1).contains("A")){
+                result[index]=1;
+            } else if (S.substring(P[index], Q[index] + 1).contains("C")){
+                result[index]=2;
+            }else if (S.substring(P[index], Q[index] + 1).contains("G")){
+                result[index]=3;
+            } else {
+                result[index]=4;
+            }
+        }
+
+        return result;
+    }
+}
--- src/utils/Printer.java
+++ src/utils/Printer.java
@@ -2,7 +2,7 @@ package utils;
 
 public class Printer {
 
-    static void printArray(int [] toPrint){
+    public static void printArray(int [] toPrint){
         for(int i : toPrint){
             System.out.print(i + ": ");
         }
