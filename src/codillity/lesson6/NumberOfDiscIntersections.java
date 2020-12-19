516f758de0061cb570e587ca1b38e30123b499a5
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-12-19 15:51:12 +0100

    Lesson 6 / NumberOfDiscIntersections / completed, not 100%

--- src/codillity/lesson6/NumberOfDiscIntersections.java
+++ src/codillity/lesson6/NumberOfDiscIntersections.java
@@ -1,45 +1,44 @@
 package codillity.lesson6;
 
+import java.util.Arrays;
+
 public class NumberOfDiscIntersections {
     public static void main(String[] args){
-        System.out.println(solution(new int []{1,5,2,1,4,0}));
+/*        System.out.println(solution(new int []{1,5,2,1,4,0}));*/
+        System.out.println(
+                solution(new int []{1, Integer.MAX_VALUE, 0}));
 
     }
 
     public static int solution(int[] A){
 
         int sumOfIntersects = 0;
-        int minRange = 0, maxRange = 0;
-        int numOfDiscs = A.length - 1;
-
-        //less than two discs
-        if(A.length < 2){
-            return sumOfIntersects;
-        }
-
+        long [] minRanges = new long[A.length];
+        long [] maxRanges = new long[A.length];
 
         //find the range of discs
         for(int i = 0; i < A.length; i++){
-            if(i-A[i]<minRange){
-                //append min range
-                minRange=i-A[i];
-            } else if(A[i]+i>maxRange){
-                //append max range
-                maxRange=i+A[i];
+            minRanges[i] = i - A[i];
+            maxRanges[i] = i + A[i];
+        }
+
+        Arrays.sort(minRanges);
+        Arrays.sort(maxRanges);
+
+        int j = 0;
+
+        for(int i = 0; i < A.length; i++){
+            while( j < A.length && maxRanges[i] >= minRanges[j]){
+                sumOfIntersects += j; // add j intersections
+                System.out.println(sumOfIntersects);
+                sumOfIntersects -= i; // minus "i" (avoid double count)
+                System.out.println(sumOfIntersects);
+                j++;
             }
         }
 
-
-        for(int j = 0; j < maxRange - 1; j++){
-            int sumOfIntersectionsPerIndex = -1;
-            for(int k = 0; k <= numOfDiscs; k++){
-                if(k-A[k]<j & k+A[k]>j){
-                    sumOfIntersectionsPerIndex++;
-                } else if (A[k]==0 & k==j){
-                    sumOfIntersectionsPerIndex++;
-                }
-            }
-            sumOfIntersects+=sumOfIntersectionsPerIndex;
+        if(sumOfIntersects > 10_000_000){
+            return -1;
         }
 
         return sumOfIntersects;
