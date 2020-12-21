9a668b81953a307c86a25dfcb8b6e28621b31f55
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-12-21 23:50:19 +0100

    Lesson 7 / StoneWall completed, not optimal.

--- src/codillity/lesson7/StoneWall.java
+++ src/codillity/lesson7/StoneWall.java
@@ -7,31 +7,47 @@ public class StoneWall {
     public static void main(String[] args){
         System.out.println(solution(new int [] {8,8,5,7,9,8,7,4,8}));
         System.out.println(solution(new int [] {3,3,3}));
-        //System.out.println(solution(new int [] {3,2,1}));
+        System.out.println(solution(new int [] {3,2,1}));
+        System.out.println(solution(new int [] {8,8,5,7,9,8}));
     }
 
     public static int solution(int[] H){
 
-        Stack<Integer> heightsToFill = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         int amountOfBlocks = 0;
-        int currentDesiredHeight = H[0];
+        int currentDesiredHeight = 0;
+
 
         int i = 0;
+        currentDesiredHeight=H[i];
         while(i < H.length){
 
+            if (H[i]>currentDesiredHeight){
+                stack.add(H[i]-currentDesiredHeight);
+            } else if (H[i] == currentDesiredHeight) {
 
-
-            if(H[i]<=currentDesiredHeight || i == H.length-1){
-
-                currentDesiredHeight=H[i];
-                amountOfBlocks++;
-                if(!heightsToFill.isEmpty()){
-                    int [] SH = heightsToFill.stream().mapToInt(j -> j).toArray();
-                    amountOfBlocks += solution(SH);
+                if(!stack.isEmpty()){
+                    amountOfBlocks += solution(stack.stream().mapToInt(j->j).toArray());
+                    stack = new Stack<>();
                 }
-            } else if(H[i]>currentDesiredHeight){
-                heightsToFill.add(H[i]-currentDesiredHeight);
+
+            } else if (H[i]<currentDesiredHeight){
+                amountOfBlocks++;
+                if(!stack.isEmpty()){
+                    amountOfBlocks += solution(stack.stream().mapToInt(j->j).toArray());
+                    stack = new Stack<>();
+                }
+                currentDesiredHeight=H[i];
             }
+
+            if(i == H.length-1){
+                if(!stack.isEmpty()){
+                    amountOfBlocks += solution(stack.stream().mapToInt(j->j).toArray());
+                    stack = new Stack<>();
+                }
+                amountOfBlocks++;
+            }
+
             i++;
         }
 
