ea2faaf72af85411ca1080eae0a6a2312003017c
Kacper Kwiatkowski <kacper18k@gmail.com>
2021-01-12 00:20:33 +0100

    Lesson 13 / FibFrog improved

--- src/codillity/lesson13/FibFrog.java
+++ src/codillity/lesson13/FibFrog.java
@@ -1,36 +1,56 @@
 package codillity.lesson13;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class FibFrog {
     public static void main(String[] args){
 
-        System.out.println(solution(new int[]{0,0,0,1,1,0,1,0,0,0,0}));
+/*        System.out.println(solution(new int[]{0,0,0,1,1,0,1,0,0,0,0}));
         System.out.println(solution(new int[]{0,0,0,0,0,0,0}));
-        System.out.println(solution(new int[]{1,1,1,1,1,1,1}));
+        System.out.println(solution(new int[]{0,0,0,0,0,1,0,0,0}));*/
+        //System.out.println(solution(new int[]{1,1,1,0,1,0,0}));
+
+        System.out.println(solution(new int[]{0,1,0,1,0,1,0,1,0,0,0,0,0,0,0}));
+        System.out.println(solution(new int[]{0,1,0,1,0,0,0,0}));
+        System.out.println(solution(new int[]{0,0,0,1,1,0,1,0,0,0,0}));
 
     }
 
     public static int solution(int [] A){
 
-        int numOfJumps = 0;
-        int distance = 1;
-        int previousJumpDistance = 0;
+        int distance = 0;
 
-        for(int i = 0; i<A.length; i++){
+        Queue<Integer> leaves = new LinkedList<>();
+
+        if(isFibNum(A.length + 1)) return 1;
+
+        for(int i = 0; i < A.length; i++){
+            distance++;
+
             if(A[i]==1){
                 if(isFibNum(distance)){
-                    distance=1;
-                    numOfJumps++;
-                    continue;
+
+                    while(!leaves.isEmpty() && isFibNum(leaves.peek() + distance)){
+                        leaves.add(leaves.poll() + distance);
+                    }
+
+                    if(!leaves.isEmpty() && isFibNum(i+1)){
+                        leaves.clear();
+                        leaves.add(i+1);
+                    } else {
+                        leaves.add(distance);
+                        distance=0;
+                    }
+
                 }
+
+                if(!leaves.isEmpty() && isFibNum(A.length + 1 - i - 1)) return leaves.size() + 1;
+
+
             }
-                distance++;
         }
 
-        if (isFibNum(distance)) return ++numOfJumps;
-        else return -1;
+        return -1;
     }
 
     public static boolean isFibNum(int N){
